608294df619210bec1f1330e24edfb8daa2ee1ab
francis-siva <fsivasithamparam@gmail.com>
2022-05-21 23:23:00 +0200

    Add InvalidSquaresDimensionOfMap class

---
+++ src/main/java/com/java_lab_corp/exceptions/InvalidSquaresDimensionOfMap.java
@@ -0,0 +1,11 @@
+package com.java_lab_corp.exceptions;
+
+public class InvalidSquaresDimensionOfMap extends RuntimeException {
+
+	private static final long serialVersionUID = -1449007511378592393L;
+
+	public InvalidSquaresDimensionOfMap(String message) {
+		super(message);
+	}
+
+}
