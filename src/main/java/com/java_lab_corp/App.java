b2ea4a6a7199c43c00a111d82b600c7e26dacf9e
francis-siva <fsivasithamparam@gmail.com>
2023-01-25 09:31:10 +0100

    Add logger dependencies & build setup changed

---
+++ src/main/java/com/java_lab_corp/App.java
@@ -0,0 +1,10 @@
+package com.java_lab_corp;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.info("TreasureMap App started");
+    }
+}
