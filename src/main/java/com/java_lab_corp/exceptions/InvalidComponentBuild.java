29cef2bfb8474373d06162bfc3122ced49c74362
francis-siva <fsivasithamparam@gmail.com>
2023-03-13 00:16:55 +0100

    Add InvalidComponentBuild Exception class

---
+++ src/main/java/com/java_lab_corp/exceptions/InvalidComponentBuild.java
@@ -0,0 +1,9 @@
+package com.java_lab_corp.exceptions;
+
+public class InvalidComponentBuild extends Exception{
+
+    public InvalidComponentBuild() { super();}
+
+    public InvalidComponentBuild(String error) { super(error);}
+
+}
