6b66bc6cfcc812dd8f7c31bf20e040e312bc8288
francis-siva <fsivasithamparam@gmail.com>
2023-03-23 19:51:28 +0100

    Treasure constructor factorized by SquareComponent inheritance

--- src/main/java/com/java_lab_corp/components/Treasure.java
+++ src/main/java/com/java_lab_corp/components/Treasure.java
@@ -1,17 +1,24 @@
 package com.java_lab_corp.components;
 
+import com.java_lab_corp.MapMD;
 import com.java_lab_corp.Square;
-import lombok.Getter;
-import lombok.Setter;
+import com.java_lab_corp.exceptions.InvalidComponentBuild;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@ToString
-public class Treasure {
-    @Getter @Setter
-    Square square;
+@ToString(callSuper = true)// required option to get whole properties in this child class toString() call
+public class Treasure extends SquareComponent {
 
 //    @Getter @Setter
 //    int nbTreasure;
+
+    public Treasure(Square square, MapMD mapMD) throws InvalidComponentBuild {
+        super(square, mapMD);
+
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
 }
